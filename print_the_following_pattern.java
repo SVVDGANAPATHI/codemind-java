f3f07d8484e27c5600cb1ce00f63182fb3018107
SVVDGANAPATHI <98637354+SVVDGANAPATHI@users.noreply.github.com>
2022-10-30 16:08:31 +0530

    import java.io.*; import java.util.*; class display {     public static void main(String args[])     {         int i,j,n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         for(i=1;i<=n;i++)         {             for(j=1;j<=i;j++)             {                 System.out.print(j);             }             System.out.println();         }
        }
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -7,11 +7,11 @@ class display
         int i,j,n;
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
-        for(i=n;i>=1;i--)
+        for(i=1;i<=n;i++)
         {
-            for(j=n;j>=1;j--)
+            for(j=1;j<=i;j++)
             {
-                System.out.print(j+" ");
+                System.out.print(j);
             }
             System.out.println();
         }
