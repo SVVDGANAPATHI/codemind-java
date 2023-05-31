a774f5e674690613aab54e106c63a010cafb2776
SVVDGANAPATHI <98637354+SVVDGANAPATHI@users.noreply.github.com>
2023-05-31 20:52:09 +0530

    import java.util.Scanner; public class aot {     public static void main(String args[])     {         Double a,e;         Scanner sc =new Scanner(System.in);          a=sc.nextDouble();          e=32+(a*9/5);          System.out.format("%.2f",e);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class aot
+{
+    public static void main(String args[])
+    { 
+        Double a,e;
+        Scanner sc =new Scanner(System.in);
+         a=sc.nextDouble();
+         e=32+(a*9/5);
+         System.out.format("%.2f",e);
+    }
+}
