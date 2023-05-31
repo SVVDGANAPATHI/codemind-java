1ca3f8fd70db99d2ca402d3443a3b8b644073fda
SVVDGANAPATHI <98637354+SVVDGANAPATHI@users.noreply.github.com>
2023-05-31 21:05:48 +0530

    import java.util.Scanner; public class aot {     public static void main(String args[])     {         char a;         int e;         Scanner sc =new Scanner(System.in);         a=sc.next().charAt(0);         e=a;         System.out.print(e);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class aot
+{
+    public static void main(String args[])
+    { 
+        char a;
+        int e;
+        Scanner sc =new Scanner(System.in);
+        a=sc.next().charAt(0);
+        e=a;
+        System.out.print(e);
+    }
+}
