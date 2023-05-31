c5eb2b988e79d08c5897f7588b8e10a2a46f5187
SVVDGANAPATHI <98637354+SVVDGANAPATHI@users.noreply.github.com>
2023-05-31 21:08:57 +0530

    import java.util.Scanner; public class aot {     public static void main(String args[])     {         Double a,b,e;         Scanner sc =new Scanner(System.in);          a=sc.nextDouble();          b=sc.nextDouble();          e=a*b;          System.out.format("%.0f",e);     } }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class aot
+{
+    public static void main(String args[])
+    { 
+        Double a,b,e;
+        Scanner sc =new Scanner(System.in);
+         a=sc.nextDouble();
+         b=sc.nextDouble();
+         e=a*b;
+         System.out.format("%.0f",e);
+    }
+}
