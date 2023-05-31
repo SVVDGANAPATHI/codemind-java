9b1c2f27e39ab6208a046b717c24a1ccab16b982
SVVDGANAPATHI <98637354+SVVDGANAPATHI@users.noreply.github.com>
2023-05-31 20:48:04 +0530

    import java.util.Scanner; public class aot {     public static void main(String args[])     {         Double a,e;         Scanner sc =new Scanner(System.in);          a=sc.nextDouble();          e=3.14*a*a;          System.out.format("%.2f",e);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class aot
+{
+    public static void main(String args[])
+    { 
+        Double a,e;
+        Scanner sc =new Scanner(System.in);
+         a=sc.nextDouble();
+         e=3.14*a*a;
+         System.out.format("%.2f",e);
+    }
+}
