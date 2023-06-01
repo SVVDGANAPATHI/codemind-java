0c1608585925df818da52ba1908f7a705b3ff707
SVVDGANAPATHI <98637354+SVVDGANAPATHI@users.noreply.github.com>
2023-06-01 11:46:42 +0530

    import java.util.Scanner; public class aot {     public static void main(String args[])     {         String a;         Scanner sc =new Scanner(System.in);          a=sc.nextLine();          System.out.format(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class aot
+{
+    public static void main(String args[])
+    { 
+        String a;
+        Scanner sc =new Scanner(System.in);
+         a=sc.nextLine();
+         System.out.format(a);
+    }
+}
