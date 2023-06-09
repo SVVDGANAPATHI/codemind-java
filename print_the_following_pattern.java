42696b79aa0828c313b7ea6a6721aa52ce4bd49f
SVVDGANAPATHI <98637354+SVVDGANAPATHI@users.noreply.github.com>
2023-06-09 14:43:21 +0530

    import java.io.*; import java.util.*; public class patter {     public static void main(String args[])     {         int n,i,j;char c;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int p=64+n;         int o=n;         for(i=0;i<n;i++)         {             for(j=0;j<o;j++)             {               System.out.print((char)p+" ");             }             o--;            System.out.println("");            p--;         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,21 +1,23 @@
 import java.io.*;
 import java.util.*;
-public class pattern
+public class patter
 {
     public static void main(String args[])
     {
-        int n,i,j,p=65;
-        char c;
+        int n,i,j;char c;
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
+        int p=64+n;
+        int o=n;
         for(i=0;i<n;i++)
         {
-            for(j=0;j<n;j++)
+            for(j=0;j<o;j++)
             {
               System.out.print((char)p+" ");
             }
+            o--;
            System.out.println(""); 
-           p++;
+           p--;
         }
     }
 }
